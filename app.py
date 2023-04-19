97e1658ff2afa95d9833d55a104dc35f65d294a0
xjming9 <981805032@qq.com>
2023-04-19 09:19:55 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/getUserInfo')
+def user():
+    return {
+        "code": "200",
+        "msg": '获取成功',
+        "name": '肖佳明'
+    }
+
+
+if __name__ == '__main__':
+    app.run()
