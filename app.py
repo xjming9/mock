ebab34e9d93d7e3afa3171dde26a7c37307f01d3
xjming9 <981805032@qq.com>
2023-04-23 10:55:15 +0800

    增加id请求方式

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,26 @@ def user():
         "name": '肖佳明',
         "age": 32
     }
+
+
+@app.route('/getUserById',method=['post'])
+def user1():
+    id = request.arg.get('id')
+    if id == "1":
+        return {
+        "code": "200",
+        "msg": '获取成功',
+        "name": '肖佳明',
+        "age": 31
+    }
+    else:
+        return {
+            "code": "500",
+            "msg": '获取失败'
+        }
+        
+    
+
 
 
 if __name__ == '__main__':
