2f2f0e1aa5ad4279385439513e5e08da59372de2
xjming9 <981805032@qq.com>
2023-05-29 15:30:18 +0800

    增加电商接口

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, request
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
@@ -35,8 +34,63 @@ def user1():
             "age": 31,
             "id": id
         }
-        
-    
+
+
+# 假设已经有一些商品数据
+products = [
+    {"id": 1, "name": "商品1", "price": 100},
+    {"id": 2, "name": "商品2", "price": 200},
+    {"id": 3, "name": "商品3", "price": 300},
+]
+
+# 获取所有商品列表
+@app.route("/api/products")
+def get_products():
+    return jsonify(products)
+
+# 根据 id 获取单个商品信息
+@app.route("/api/products/<int:product_id>")
+def get_product(product_id):
+    for product in products:
+        if product["id"] == product_id:
+            return jsonify(product)
+    # 如果找不到则返回 404 Not Found 错误
+    return jsonify({"error": "Product not found"}), 404
+
+# 添加新的商品
+@app.route("/api/products", methods=["POST"])
+def add_product():
+    data = request.get_json()
+    if not data or not data.get("name") or not data.get("price"):
+        return jsonify({"error": "Invalid data"})
+    new_id = max([product["id"] for product in products]) + 1
+    new_product = {"id": new_id, "name": data["name"], "price": data["price"]}
+    products.append(new_product)
+    return jsonify(new_product)
+
+# 修改一个商品的信息
+@app.route("/api/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid data"})
+    for product in products:
+        if product["id"] == product_id:
+            product.update(data)
+            return jsonify(product)
+    # 如果找不到则返回 404 Not Found 错误
+    return jsonify({"error": "Product not found"}), 404
+
+# 删除一个商品
+@app.route("/api/products/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    for i, product in enumerate(products):
+        if product["id"] == product_id:
+            products.pop(i)
+            return jsonify({"message": f"Product {product_id} has been deleted"})
+    # 如果找不到则返回 404 Not Found 错误
+    return jsonify({"error": "Product not found"}), 404
+
 
 
 
